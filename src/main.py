6ca688db2b147a2456a5f0e7c08b47fe8ee4d54c
Patrick Bonnell <plbonnell538@gmail.com>
2019-01-02 15:39:28 -0500

    minor changes
    Signed-off-by: Patrick Bonnell <plbonnell538@gmail.com>

--- src/main.py
+++ src/main.py
@@ -1,15 +1,20 @@
 #!/usr/bin/python
 
-from pandas_datareader import data as pdr
-
 import fix_yahoo_finance as y
+from pandas_datareader import data as pdr
+import traceback as tb
 
 y.pdr_override()
 
 
 if __name__ == '__main__':
 
-	data = pdr.get_data_yahoo('AMD', start='2017-01-01', end='2018-01-01').to_csv('test.csv')
-	print(data)
+	while True:
+		try:
+			data = pdr.get_data_yahoo('AMD', start='2017-01-01', end='2018-01-01')
+		except ValueError:
+			pass
+		else:
+			break
 
-
+	data = data.to_csv('test.csv')
